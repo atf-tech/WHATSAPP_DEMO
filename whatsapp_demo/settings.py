ed7c3594b53d8993287ada483f11e5cfca68071e
SivakumarATF <sivakumaratf@gmail.com>
2026-01-23 14:15:58 +0530

    mysql

--- whatsapp_demo/settings.py
+++ whatsapp_demo/settings.py
@@ -85,10 +85,14 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'whatsapp_demo1',
+    'USER': 'whatsapp_user',
+    'PASSWORD': 'atf_tech',
+    'HOST': 'localhost',
+    'PORT': '3306',
+  }
 }
 
 
