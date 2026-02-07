1eb46dccf0753e9a8594bd3144fdae3a5791abc5
SivakumarATF <sivakumaratf@gmail.com>
2026-02-07 16:49:00 +0530

    update

--- whatsapp_demo/urls.py
+++ whatsapp_demo/urls.py
@@ -1,27 +1,20 @@
-"""
-URL configuration for whatsapp_demo project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import os
+from django.http import FileResponse, Http404
+
+def service_worker(request):
+    path = os.path.join(settings.BASE_DIR, "service-worker.js")
+    if not os.path.exists(path):
+        raise Http404("service-worker.js not found")
+    return FileResponse(open(path, "rb"), content_type="application/javascript")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("accounts.urls")),
     path("whatsapp/", include("whatsapp.urls")),
     path("chat/", include("chat.urls")),
+    path("service-worker.js", service_worker),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
